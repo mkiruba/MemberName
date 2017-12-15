dac7f8fadac2a49f7257153fd0269e99a29bc923
mkiruba <mkiruba@gmail.com>
2017-12-15 00:31:58 +0000

    added mahapps for UI styling

--- MemberName/Frontend/MemberControl.xaml.cs
+++ MemberName/Frontend/MemberControl.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
+using System.Windows;
 
 namespace MemberName.Frontend
 {
     /// <summary>
     /// Interaction logic for MemberControl.xaml
     /// </summary>
-    public partial class MemberControl : Window
+    public partial class MemberControl : MetroWindow
     {
         public MemberControl()
         {
